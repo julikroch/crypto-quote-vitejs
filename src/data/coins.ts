73dbc649e58bcbc135b648800080d8f77c821a12
Julian Krochmal <juliankrochmal@gmail.com>
2022-07-07 09:40:29 -0300

    adding all the components

---
+++ src/data/coins.ts
@@ -0,0 +1,6 @@
+export const coins = [
+    { id: 'USD', nombre: 'Dolar de Estados Unidos' },
+    { id: 'MXN', nombre: 'Peso Mexicano' },
+    { id: 'EUR', nombre: 'Euro' },
+    { id: 'GBP', nombre: 'Libra Esterlina' },
+]
